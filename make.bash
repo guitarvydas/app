051ff010f60643b14da87e1fe498a8ae5ad4a7ba
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-02 08:12:51 -0500

    cp all tools in make.bash ; added facts for root and mxGraphModel

--- make.bash
+++ make.bash
@@ -1,6 +1,23 @@
 echo '** copying commands **'
 app=${HOME}/app
 
+# pfr and pf
+cp ${app}/pfr/pfr ${app}/bin
+chmod a+x ${app}/bin/pfr
+
+cp ${app}/pfr/pf ${app}/bin
+chmod a+x ${app}/bin/pf
+
+
+# d2f (d2x for debugging)
+cp ${app}/d2f/d2f ${app}/bin
+chmod a+x ${app}/bin/d2f
+
+cp ${app}/d2f/d2x ${app}/bin
+chmod a+x ${app}/bin/d2x
+
+
+# querydisplay, querydisplay2, setquerydisplay
 cp ${app}/querydisplay/querydisplay ${app}/bin
 chmod a+x ${app}/bin/querydisplay
 
