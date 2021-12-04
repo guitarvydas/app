039a9cb8129fd62b8eeb46b03d72b0806ce58599
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-04 18:13:45 -0500

    begin design rule

--- pre/dev.bash
+++ pre/dev.bash
@@ -10,6 +10,13 @@ cdir=`pwd`
 # node pre.js '#+ forall ' forall.ohm forall.glue $cdir/support.js <in.txt
 # negative case:: node pre.js '#+ xyz ' forall.ohm forall.glue $cdir/support.js <in.txt
 
-app=${HOME}/app
-${app}/make.bash
-pre '#+ forall ' forall.ohm forall.glue $cdir/support.js <in.txt
+# app=${HOME}/app
+# ${app}/make.bash
+# pre '#+ forall ' forall.ohm forall.glue $cdir/support.js <in.txt
+
+echo forall
+node pre.js '#+ forall ' forall.ohm forall.glue ${cdir}/support.js <in.txt >temp
+echo design rule
+node pre.js '#+ design rule ' designrule.ohm designrule.glue ${cdir}/support.js <temp
+
+
