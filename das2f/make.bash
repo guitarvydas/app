dbc70155c259801359e955b322db1476f6af5ccd
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-11 16:28:14 -0500

    cp before converting all querydisplay2 to querydisplay3

--- das2f/make.bash
+++ das2f/make.bash
@@ -58,16 +58,12 @@ querydisplay2 component >layerfinal_query.bash
 chmod a+x layerfinal_query.bash
 echo '-- layerfinal_query.bash generated --'
 
-echo new version kind 1
 pre '#+ forall ' '#+ ' forall.ohm forall.glue <layerkind.md >preprocessed_layerkind.md
-echo new version kind 2
 querydisplay3 preprocessed_layerkind >layerkind_query.bash
 chmod a+x layerkind_query.bash
 echo '-- layerkind_query.bash generated --'
 
-echo new version for color 1
 pre  "#+ forall " "#+ " forall.ohm forall.glue <layercolor.md >preprocessed_layercolor.md
-echo new version for color 2
 querydisplay3 preprocessed_layercolor >layercolor_query.bash
 chmod a+x layercolor_query.bash
 echo '-- layercolor_query.bash generated --'
