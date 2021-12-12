200944bcc1dc9e7b004af293d8463ea2d6c29873
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-12 01:20:34 -0500

    wip: python3 tests

--- das2j/test.py
+++ das2j/test.py
@@ -6,4 +6,22 @@ with open('out.json') as f:
 
 #print(data)
 #print(json.dumps(data, indent = 4, sort_keys=True))
-print(json.dumps(data, indent = 2))
+#print(json.dumps(data, indent = 2))
+
+# print (len(data))
+
+for componentArray in data:
+  #print(componentArray, end="\n\n")
+  for component in componentArray:
+    print ("name: "     + component["name"])
+    print ("inputs: "   , end="")
+    print (component["inputs"], end="\n")
+    print ("outputs: ", end="")
+    print (component["outputs"], end="\n")
+    print ("children: ", end="")
+    print (component["children"], end="\n")
+    print ("connections: ", end="")
+    for conn in component["connections"]:
+      print (conn, end="")
+    print ("\n")
+  
