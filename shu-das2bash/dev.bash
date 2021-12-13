361e33ace0c6c47d04a6e0e3c44e2d69bda6fb30
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-13 18:15:46 -0500

    unescapes

--- shu-das2bash/dev.bash
+++ shu-das2bash/dev.bash
@@ -2,5 +2,9 @@
 ../make.bash
 das2j helloworld >out.json
 python3 emit.py
+echo
+cat hello.bash
+echo
+cat world.bash
 
 
--- shu-das2bash/emit.py
+++ shu-das2bash/emit.py
@@ -1,24 +1,27 @@
 
 import json
+import html
+import re
 
 with open('out.json') as f:
   data = json.load(f)
 
+def printScript (component, outf):
+  code = html.unescape (component["synccode"])
+  code2 = re.sub (r'<div>([^<]*)</div>', r'\1\n', code)
+  code3 = re.sub (r'<p ([^>]*)>', r'', code2)
+  code4 = re.sub (r'</p>', "", code3)
+  code5 = re.sub (r'<span ([^<]*)>', "", code4)
+  code6 = re.sub (r'</span>', "\n", code5)
+  code7 = re.sub (r'<br>', "\n", code6)
+
+  codefinal = html.unescape (code7)
+  print (codefinal, file=outf)
+
 for componentArray in data:
   for component in componentArray:
-    print ("name: "     + component["name"])
-    print ("inputs: "   , end="")
-    print (component["inputs"], end="\n")
-    print ("outputs: ", end="")
-    print (component["outputs"], end="\n")
-    print ("children: ", end="")
-    print (component["children"], end="\n")
-    if (0 == len(component["connections"])):
-      print ("connections: []", end="\n")
-    else:
-      print ("connections: ", end="\n")
-      for conn in component["connections"]:
-        print ("  ", end="")
-        print (conn, end="\n")
-    print ("\n")
-  
+    fname = component["name"] + ".bash"
+    with open (fname, "w") as script:
+      print ("# ", file=script, end="")
+      print (fname, file=script, end="\n")
+      printScript (component, script)
