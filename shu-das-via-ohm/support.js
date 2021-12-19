533f2089a816a3b42e71f5b1be6611b2bc35aa37
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-19 10:49:16 -0500

    wip: names

--- shu-das-via-ohm/support.js
+++ shu-das-via-ohm/support.js
@@ -1,25 +1,47 @@
 // registers
 var fileName;
 var componentName;
+var childrenNames;
+var inputNames;
+var outputNames;
 
 exports.resetRegisters = function () {
     fileName = "";
     componentName = "";
-    return "";
+    childrenNames = "";
+    inputNames = "";
+    outputNames = "";
 }
 
 exports.fileNameSet = function (s) {
     fileName = s;
-    return "";
 }
 
 exports.componentNameSet = function (s) {
     componentName = s;
-    return "";
+}
+
+function deleteLeadingComma (s) {
+    return s.replace (/, /, '');
+}
+
+exports.childrenNamesSet = function (s) {
+    childrenNames = deleteLeadingComma (s);
+}
+
+exports.inputNamesSet = function (s) {
+    inputNames = deleteLeadingComma (s);
+}
+
+exports.outputNamesSet = function (s) {
+    inputNames = deleteLeadingComma (s);
 }
 
 exports.writeComponent = function () {
     console.error (`filename = ${fileName}`);
     console.error (`componentName = ${componentName}`);
+    console.error (`childrenNames = [${childrenNames}]`);
+    console.error (`inputNames = [${inputNames}]`);
+    console.error (`outputNames = [${outputNames}]`);
     console.error ();
 }
