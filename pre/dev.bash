9125ae3cb17c9cd44b096ed6a5c9f329850b9e69
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-26 16:15:47 -0500

    up to test4 of new pre

--- pre/dev.bash
+++ pre/dev.bash
@@ -2,5 +2,12 @@
 app=${HOME}/app
 ${app}/make.bash
 cdir=`pwd`
-echo test1
-pre 'a' 'c' test1.ohm test1.glue <test1
+echo running test1
+pre 'a' 'c' test1.ohm test1.glue --input=test1
+#pre 'a' 'c' test1.ohm test1.glue <test1
+echo running test2
+pre 'ab' 'c' test2.ohm test2.glue --input=test2
+echo running test3
+pre 'ab' 'c' test2.ohm test2.glue --input=test3
+echo running test4
+pre 'ab' 'ab|c' test2.ohm test2.glue --input=test4
