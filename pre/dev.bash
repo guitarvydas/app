951def30ce854d42b1eb2be6bb49bb997373be52
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-27 07:54:33 -0500

    newpre

--- pre/dev.bash
+++ pre/dev.bash
@@ -1,20 +1,2 @@
-# # # pfr forall.txt forall.ohm forall.glue support.js
-app=${HOME}/app
-${app}/make.bash
-cdir=`pwd`
-echo running test1 '(should be *c)'
-pre 'a' 'c' test1.ohm test1.glue --input=test1
-#pre 'a' 'c' test1.ohm test1.glue <test1
-echo running test2 '(should be *c)'
-pre 'ab' 'c' test2.ohm test2.glue --input=test2
-echo running test3 '(should be *c*c)'
-pre 'ab' 'c' test2.ohm test2.glue --input=test3
-echo running test4 '(should be ***c)'
-pre 'ab' 'ab|c' test2.ohm test2.glue --input=test4
-echo running test5 '(should be ***c)'
-pre '.' '$' test5.ohm test5.glue --stop=1 --input=test5
-echo running test forall
-pre '#+ forall' '#+ ' forall.ohm forall.glue --input=testforall --support=${cdir}/support.js
-echo running test implicit forall
-pre '#+ query' '#+ ' implicitforall.ohm implicitforall.glue --input=testimplicitforall --support=${cdir}/support.js >temp
-sed -E -e 's/~/query\n/g' <temp
+echo 'see run.bash for examples, use "pre" at command line'
+exit 1
