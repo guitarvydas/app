81bd35fad31764f2238a6bb25d7ad6fdaf5e3d15
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-31 14:46:03 -0500

    fixed dr, js code declares failure

--- das2f/dev.bash
+++ das2f/dev.bash
@@ -1,39 +1,30 @@
-echo use run.bash instead
-exit 1
+#echo use run.bash instead
+#exit 1
 
-# this is only an example
-# use das2f instead
-clear
-#./build.bash
-#./run-fb-pipeline.bash helloworld
-das2f helloworld
 
-# ../make.bash
-# cwd=`pwd`
-# das2fdir=`pwd`
-# temp=temp_${RANDOM}
+../make.bash
+cwd=`pwd`
+das2fdir=`pwd`
+temp=temp_${RANDOM}
 
-# plpath=./
-# dr=~/projects/dr
-# mdfile=${dr}/dr-edgecontainment.md
-# fname=`basename -s '.md' $mdfile`
-# prep "cond\n" "endcond\n" ${dr}/cond.ohm ${dr}/cond.glue --inclusive --stop=1 --support=${dr}/drsupport.js <$mdfile >$temp
-# prep "." "$" ${dr}/designrule.ohm ${dr}/designrulea.glue --stop=1 --support=${dr}/drsupport.js --PLPATH=$plpath<$temp >a-$fname
-# prep "." "$" ${dr}/designrule.ohm ${dr}/designruleb.glue --stop=1 --support=${dr}/drsupport.js <$mdfile >b-$fname
-# chmod a+x a-$fname
-# chmod a+x b-$fname
-# echo '-- ' "design rules a-${fname} and b-${fname} generated" ' --' 1>&2
+dr=~/projects/dr
+mdfile=${dr}/dr-edgecontainment.md
+fname=`basename -s '.md' $mdfile`
+temp=temp_${RANDOM}
 
-# ./a-${fname} | ./b-${fname} 2> $temp
-# #./a-${fname}
+#./a-${fname} | ./b-${fname} 2> $temp
+./a-${fname} >$temp
+cat $temp
+./b-${fname} <$temp
 
-# #./check-errors.bash
-# if grep -q failure <$temp
-# then
-#     echo
-#     cat $temp 1>&2
-#     echo quitting 1>&2
-#     rm $temp
-#     exit 1
-# fi
-# rm $temp
+#./check-errors.bash
+if grep -q failure <$temp
+then
+    echo
+    cat $temp 1>&2
+    echo quitting 1>&2
+    rm $temp
+    exit 1
+fi
+rm -f $temp
+
