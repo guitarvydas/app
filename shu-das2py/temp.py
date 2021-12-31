41f1c4039783daa9db05f798de0f1ab25abefed1
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-31 15:51:34 -0500

    samplehelloworld.py rename _ to p_

--- shu-das2py/temp.py
+++ shu-das2py/temp.py
@@ -14,7 +14,7 @@ class _helloworld (mpos.Container):
 
         child0 = world._world (dispatcher, self, 'world')
         child1 = hello._hello (dispatcher, self, 'hello')
-        conn0 = mpos.Connector ([mpos.Sender ('', '_')], [mpos.Receiver ('hello', '_')])
+        conn0 = mpos.Connector ([mpos.Sender ('', 'p_')], [mpos.Receiver ('hello', 'p_')])
         conn1 = mpos.Connector ([mpos.Sender ('hello', 'out')], [mpos.Receiver ('world', 'in')])
         self.connections = [ conn0, conn1 ]
         self.children = {'world':child0, 'hello':child1}
